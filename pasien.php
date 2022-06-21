33620e14b4c2c5a21eebaf64a49c46ef6eef5c03
zikri hadiansyah <zikri.hadiansyah21@gmail.com>
2022-06-21 18:22:35 +0700

    menambahkan file pasien.php

---
+++ pasien.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+    <title>pasien</title>
+	<style>
+		table {
+            color: black;
+			background-color: #F5F5F5;
+		}
+        table,tr,td {
+            border: 1px solid black;
+			margin: auto;
+			padding: 12px;
+		}
+        thead {
+            background-color: #1f5faa;
+			color: white;
+			font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+<div id="container">
+        <header>
+            <h1 align="center">Data Pasien</h1>
+        </header>
+		<nav>
+            <a href="index.php">Home</a>
+            <a href="pasien.php"class="active">Pasien</a>
+            <a href="obat.php">Obat</a>
+            <a href="dokter.php">Dokter</a>
+            <a href="berobat.php">Berobat</a>
+			<a href="resep.php">Resep</a>
+			<a href="user.php">User</a>
+        </nav>
+	<div id="hero">
+	<h2 align="center">Tabel Pasien</h2>
+	<br>
+    <table class="table">
+        <thead>
+            <tr>
+                <td>No</td>
+				<td>Id Pasien</td>
+                <td>Nama Pasien</td>
+                <td>Jenis Kelamin</td>
+                <td>Umur</td>                
+            </tr>
+        </thead>
+        <?php
+        include "koneksi.php";
+        $no = 1;
+        $query = mysqli_query($conn, 'SELECT * FROM pasien');
+        while ($data = mysqli_fetch_array($query)) 
+        {
+        ?>
+            <tr>
+                <td><?php echo $no++ ?></td>
+				<td><?php echo $data['id_pasien'] ?></td>
+                <td><?php echo $data['nama_pasien'] ?></td>
+                <td><?php echo $data['jenis_kelamin'] ?></td>
+                <td><?php echo $data['umur'] ?></td>
+            </tr>
+        <?php 
+        } ?>
+    </table>
+	</div>
+    <footer>
+		<p>&copy; 2022 - Universitas Pelita Bangsa</p>
+	</footer>
+</div>
+</body>
+</html>
