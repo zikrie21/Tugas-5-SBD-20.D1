528dac25d06b54d854c72a2b58bc15acf9926aa1
zikri hadiansyah <zikri.hadiansyah21@gmail.com>
2022-06-21 18:20:41 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+    <title>Home</title>
+	<style>
+        table,tr,td {
+            border: 1px solid black;
+			padding: 10px 15px;
+        }
+        thead {
+            background-color: #cccddd;
+        }
+    </style>
+</head>
+<body>
+<div id="container">
+        <header>
+            <h1 align="center">Sistem Informasi Klinik</h1>
+        </header>
+		<nav>
+			<a href="index.php"class="active">Home</a>
+            <a href="pasien.php">Pasien</a>
+            <a href="obat.php">Obat</a>
+            <a href="dokter.php">Dokter</a>
+            <a href="berobat.php">Berobat</a>
+			<a href="resep.php">Resep</a>
+			<a href="user.php">User</a>
+        </nav>
+		<section id="hero">
+		<h1>Selamat Datang</h1>
+        <p>Website ini berisi tentang Sistem Informasi Klinik untuk memenuhi Tugas Sistem Basis Data Zikri Hadiansyah.</p>
+		<a href="home.html" class="btn btn-large">Learn more &raquo;</a>
+		</section>
+	<footer>
+		<p>&copy; 2022 - Universitas Pelita Bangsa</p>
+	</footer>
+</div>
+</body>
+</html>
